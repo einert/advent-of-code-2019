5fb2d67e7df144dfe264ca49f0ba3d720f52e50d
Einar Rune Haugnes <einarrune.haugnes@gmail.com>
2019-12-10 00:30:24 +0100

    First try - 3303995

--- day1/fuel-counter-upper/src/fuel_counter_upper/core.clj
+++ day1/fuel-counter-upper/src/fuel_counter_upper/core.clj
@@ -1,7 +1,21 @@
 (ns fuel-counter-upper.core
   (:gen-class))
 
+(require '[clojure.java.io :as io])
+
+(defn process-file-with [line-func line-acc filename]
+  (with-open [rdr (io/reader (io/file filename))]
+    (reduce line-func line-acc 
+            (map #(- (quot % 3) 2) 
+                 (map read-string 
+                      (line-seq rdr))))))
+
+;;(require '[clojure.string :as string])
+
+(defn accumulate [acc fuel]
+  (+ acc fuel))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (prn process-file-with accumulate 0 "input"))
