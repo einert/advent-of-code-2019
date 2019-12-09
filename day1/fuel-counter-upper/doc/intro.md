390bb9c8adc1c02e3ef889adec570ec7a293f3a2
Einar Rune Haugnes <einarrune.haugnes@gmail.com>
2019-12-09 20:32:39 +0100

    Add standard top gitignore, project for d1

---
+++ day1/fuel-counter-upper/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fuel-counter-upper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ day1/fuel-counter-upper/src/fuel_counter_upper/core.clj
@@ -0,0 +1,7 @@
+(ns fuel-counter-upper.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
