7d280af18d82f62090ce6971ea029061c6330c9d
Einar Rune Haugnes <ehaugnes@slb.com>
2019-12-10 12:40:46 +0100

    Inspired by idiomatic tweet from  @jaihindreddy

--- day1/fuel-counter-upper/test/fuel_counter_upper/core_test.clj
+++
@@ -1,9 +0,0 @@
-(ns fuel-counter-upper.core-test
-  (:require [clojure.test :refer :all]
-            [fuel-counter-upper.core :refer :all :as sut]))
-
-(deftest fuel
-  (testing "Check total-fuel for a number of test module weights"
-    (is (= (sut/total-fuel 100756) 50346))
-    (is (= (sut/total-fuel 1969) 966))
-    (is (= (sut/total-fuel 14) 2))))
