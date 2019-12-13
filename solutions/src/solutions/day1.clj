e9a7dfa606a653815b47713c7b6e32c79aaf2119
Einar Rune Haugnes <einarrune.haugnes@gmail.com>
2019-12-13 03:24:46 +0100

    Major restrcture, part1 done w star. Need lein fix

--- solutions/src/solutions/day1.clj
+++ solutions/src/solutions/day1.clj
@@ -1,15 +1,15 @@
-(ns fuel-counter-upper.core
-  (:gen-class))
+(ns advent2019.day1)
 
 (require '[clojure.java.io :as io])
 
 (defn process-file [filename]
+  (prn (str "Filename: " filename))
   (let [fuel-one #(- (quot % 3) 2)
         total-fuel #(->> (iterate fuel-one %)
                          (take-while pos?)
                          (rest)
                          (reduce +))]
-    (with-open [reader (io/reader (io/file filename))]
+    (with-open [reader (io/reader (io/resource filename))]
       (->> (line-seq reader)
            (map read-string)
            (map total-fuel)
@@ -18,4 +18,4 @@
 (defn -main
   "On the first day, do the thing with the fuel"
   [& args]
-  (prn (process-file "input")))
+  (prn (process-file "day1.txt")))
--- solutions/src/solutions/day2.clj
+++ solutions/src/solutions/day2.clj
@@ -36,3 +36,9 @@
   (t/is (= [2 4 4 5 99 9801] (computer [2 4 4 5 99 0])))
   (t/is (= [30 1 1 4 2 5 6 0 99] (computer [1 1 1 4 99 5 6 0 99])))
   )
+
+(defn part1
+  []
+  (computer (-> input
+                (assoc 1 12)
+                (assoc 2 2))))
