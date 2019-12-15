d0f9f85aa21b5a4286514db1c92694f36d9f99f9
Einar Rune Haugnes <einarrune.haugnes@gmail.com>
2019-12-15 02:38:40 +0100

    Part 2 and tests done.

--- src/advent2019/day5.clj
+++ src/advent2019/day5.clj
@@ -11,7 +11,7 @@
 (t/with-test
 
   (defn execute
-    ([program] (execute program nil)) 
+    ([program] (execute program nil))
     ([program input-num]
      (loop [memory program
             i 0
@@ -27,16 +27,56 @@
                       (get memory (+ i 2))))
              target (get memory (+ i 3))]
          (case (mod op 100)
-           1 (recur (assoc memory target (+ a b)) (+ i 4) result)
-           2 (recur (assoc memory target (* a b)) (+ i 4) result)
-           3 (recur (assoc memory (get memory (inc i)) input-num) (+ i 2) result)
-           4 (recur memory (+ i 2) (conj result a))
-           99 [memory (last result)])))))
-
-
+           1 (recur (assoc memory target (+ a b)) (+ i 4) result) ; add
+           2 (recur (assoc memory target (* a b)) (+ i 4) result) ; mult
+           3 (recur (assoc memory (get memory (inc i)) input-num) (+ i 2) result) ; get input
+           4 (recur memory (+ i 2) (conj result a)) ; write output
+           5 (recur memory (if (not= 0 a) b (+ i 3)) result) ; jump-if-true
+           6 (recur memory (if (= 0 a) b (+ i 3)) result) ; jump-if-false
+           7 (recur (assoc memory target (if (< a b) 1 0)) (+ i 4) result) ; less-than
+           8 (recur (assoc memory target (if (= a b) 1 0)) (+ i 4) result) ; equals
+           99 [memory (last result)]))))) ; quit
+  
+  ;; test-o-rama
   (t/is (= [2 0 0 0 99] (first (execute [1 0 0 0 99]))))
   (t/is (= [2 3 0 6 99] (first (execute [2 3 0 3 99]))))
   (t/is (= [2 4 4 5 99 9801] (first (execute [2 4 4 5 99 0]))))
   (t/is (= [30 1 1 4 2 5 6 0 99] (first (execute [1 1 1 4 99 5 6 0 99]))))
-  
+  (t/is (= [1002 4 3 4 99]) (first (execute [1002 4 3 4 33])))
+  (t/is (= [1101 100 -1 4 -100]) (first (execute [1101 100 -1 4 0])))
+  (t/is (= 0) (second (execute [3 9 8 9 10 9 4 9 99 -1 8] 7)))
+  (t/is (= 1) (second (execute [3 9 8 9 10 9 4 9 99 -1 8] 8)))
+  (t/is (= 0) (second (execute [3 9 7 9 10 9 4 9 99 -1 8] 9)))
+  (t/is (= 1) (second (execute [3 9 7 9 10 9 4 9 99 -1 8] 7)))
+  (t/is (= 0) (second (execute [3 3 1108 -1 8 3 4 3 99] 7)))
+  (t/is (= 1) (second (execute [3 3 1108 -1 8 3 4 3 99] 8)))
+  (t/is (= 0) (second (execute [3 3 1107 -1 8 3 4 3 99] 9)))
+  (t/is (= 1) (second (execute [3 3 1107 -1 8 3 4 3 99] 7)))
+  (t/is (= 0) (second (execute [3 12 6 12 15 1 13 14 13 4 13 99 -1 0 1 9] 0)))
+  (t/is (= 1) (second (execute [3 12 6 12 15 1 13 14 13 4 13 99 -1 0 1 9] 42)))
+  (t/is (= 0) (second (execute [3 3 1105 -1 9 1101 0 0 12 4 12 99 1] 0)))
+  (t/is (= 1) (second (execute [3 3 1105 -1 9 1101 0 0 12 4 12 99 1] 42)))
+
+  (t/is (= 999)  (second (execute [3 21 1008 21 8 20 1005 20 22 107 8
+                                   21 20 1006 20 31 1106 0 36 98 0 0
+                                   1002 21 125 20 4 20 1105 1 46 104
+                                   999 1105 1 46 1101 1000 1 20 4 20
+                                   1105 1 46 98 99] 7)))
+  (t/is (= 1000) (second (execute [3 21 1008 21 8 20 1005 20 22 107 8
+                                   21 20 1006 20 31 1106 0 36 98 0 0
+                                   1002 21 125 20 4 20 1105 1 46 104
+                                   999 1105 1 46 1101 1000 1 20 4 20
+                                   1105 1 46 98 99] 8)))
+  (t/is (= 1001) (second (execute [3 21 1008 21 8 20 1005 20 22 107 8
+                                   21 20 1006 20 31 1106 0 36 98 0 0
+                                   1002 21 125 20 4 20 1105 1 46 104
+                                   999 1105 1 46 1101 1000 1 20 4 20
+                                   1105 1 46 98 99] 9)))
   )
+
+(defn part1 []
+  (execute input 1))
+
+(defn part2 []
+  (execute input 5))
+
