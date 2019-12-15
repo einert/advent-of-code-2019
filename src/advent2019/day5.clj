931ce49438ee86ce6157f9a23db544e027127cee
Einar Rune Haugnes <einarrune.haugnes@gmail.com>
2019-12-15 01:45:05 +0100

    Adapted fdside's loop/recur based solution + tests

--- src/advent2019/day5.clj
+++ src/advent2019/day5.clj
@@ -1,80 +1,42 @@
 (ns advent2019.day5
-  (:require [clojure.test :as t]
-            [clojure.string :as string]
-            [clojure.java.io :as io]))
+  (:require [clojure.test :as t]))
 
-;; Solution from elatedpixel was a real eye-opener
-;; Borrowed/copied shamelessly from him to play with the
-;; code and find out how it works.
-;; Simplified away the map, which seems like overkill.
+;; Mostly stolen from fdside's awesome implementation
+;; https://github.com/fdside/advent-of-code-2019-clj/blob/master/src/advent_of_code_2019/day5.clj
 
-(def input
-  (->> "day2.txt"
-       io/resource
-       io/reader
-       line-seq
-       first
-       (format "[%s]")
-       read-string))
-
-(defn print-fl [& messages]
-  (apply print messages) 
-  (flush)) 
-
-(defn get-input []
-  (print-fl "input> ")
-  (read-string (string/trim-newline (read-line))))
-
-(defn write-output [output]
-  (println (str "output> " output)))
-
-(def opcode
-  {1 +
-   2 *
-   3 get-input
-   4 write-output})
+(def input (vec (map 
+                 read-string 
+                 (clojure.string/split (slurp "resources/day5.txt") #","))))
 
 (t/with-test
 
-  (defn computer
-    ([program] (reduce computer program (range)))
-    ([program i]
-     (let [stop 99
-           nop -1
-           [op a b r] (nth (partition-all 4 4 program) i)]
-       (println (str "Program " program " it " i))
-       (println (str "Destruct " [op a b r]))
-       (if (= stop op)
-         (reduced program) ; all done, bail out of reduce loop
-         (cond 
-           (< 3 op) (assoc program r 
-                           ((opcode op) (program a) (program b)))
-           (= 3 op) (->> program
-                         (#(if (= nop (first %)) (into [] (drop 2 %)) %))
-                         (#(assoc % a ((opcode op))))
-                         (into [nop nop]))
-           (= 4 op) (->> program
-                         (#((opcode op) (% a)))
-                         (into [nop nop])))
-         ))))
+  (defn execute
+    ([program] (execute program nil)) 
+    ([program input-num]
+     (loop [memory program
+            i 0
+            result []]
+       (let [op (get memory i)
+             a (get memory
+                    (if (= 1 (mod (quot op 100) 10)) ; mode - immediate or dereference
+                      (+ i 1)
+                      (get memory (+ i 1))))
+             b (get memory
+                    (if (= 1 (mod (quot op 1000) 10))
+                      (+ i 2)
+                      (get memory (+ i 2))))
+             target (get memory (+ i 3))]
+         (case (mod op 100)
+           1 (recur (assoc memory target (+ a b)) (+ i 4) result)
+           2 (recur (assoc memory target (* a b)) (+ i 4) result)
+           3 (recur (assoc memory (get memory (inc i)) input-num) (+ i 2) result)
+           4 (recur memory (+ i 2) (conj result a))
+           99 [memory (last result)])))))
 
-  (t/is (= [2 0 0 0 99] (computer [1 0 0 0 99])))
-  (t/is (= [2 3 0 6 99] (computer [2 3 0 3 99])))
-  (t/is (= [2 4 4 5 99 9801] (computer [2 4 4 5 99 0])))
-  (t/is (= [30 1 1 4 2 5 6 0 99] (computer [1 1 1 4 99 5 6 0 99]))))
 
-(defn part1
-  []
-  (computer (-> input
-                (assoc 1 12)
-                (assoc 2 2))))
-
-(defn part2
-  []
-  (some #(when (= 19690720 (second %)) (first %))
-        (pmap (fn [[a b]]
-                [(+ b (* 100 a))
-                 (first (computer (-> input
-                                      (assoc 1 a)
-                                      (assoc 2 b))))])
-              (for [a (range 100) b (range 100)] [a b]))))
+  (t/is (= [2 0 0 0 99] (first (execute [1 0 0 0 99]))))
+  (t/is (= [2 3 0 6 99] (first (execute [2 3 0 3 99]))))
+  (t/is (= [2 4 4 5 99 9801] (first (execute [2 4 4 5 99 0]))))
+  (t/is (= [30 1 1 4 2 5 6 0 99] (first (execute [1 1 1 4 99 5 6 0 99]))))
+  
+  )
